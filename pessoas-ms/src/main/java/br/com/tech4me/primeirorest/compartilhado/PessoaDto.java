233516db2512548830d969bd05b0e64dae493b42
luiz <luiz_clsx@hotmail.com>
2021-08-29 22:46:42 -0300

    Mciroserviços

---
+++ pessoas-ms/src/main/java/br/com/tech4me/primeirorest/compartilhado/PessoaDto.java
@@ -0,0 +1,38 @@
+package br.com.tech4me.primeirorest.compartilhado;
+
+import java.util.List;
+
+public class PessoaDto {
+    private String id;
+    private String nome;
+    private String sobrenome;
+    
+    //#region Get / Set
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    //#endregion
+
+    public void setAnimais(List<Animal> obterAnimais) {
+    }
+}
---
+++ pessoas-ms/src/main/java/br/com/tech4me/primeirorest/view/model/PessoaModeloResponseDetalhes.java
@@ -0,0 +1,40 @@
+package br.com.tech4me.primeirorest.view.model;
+
+import java.util.List;
+
+import br.com.tech4me.primeirorest.compartilhado.Animal;
+
+public class PessoaModeloResponseDetalhes {
+
+    private String id;
+    private String nome;
+    private String sobrenome;
+    private List<Animal> animal;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public List<Animal> getAnimal() {
+        return animal;
+    }
+    public void setAnimal(List<Animal> animal) {
+        this.animal = animal;
+    }
+    
+
+}
